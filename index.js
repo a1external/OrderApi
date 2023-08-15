ebf90d0c1090e68a6ff167232fa10fe54cbed459
mujteba Khan <mujteba.a1external.com>
2023-08-15 10:37:16 -0700

    change and commit

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ let ordersInfo = [];
 
 app.post('/orders', (req, res) => {
   const requestData = req.body;
-
+  console.log("Response sent.");
   try {
     ordersInfo.push(requestData);
     fs.writeFileSync('orders.json', JSON.stringify(ordersInfo, null, 2), 'utf-8');
-  
     res.status(200).json({ message: 'Order received successfully', data: requestData });
-    console.log("Response sent.");
+   
     console.log(requestData);
+    
+   
   } catch (error) {
     console.error("Error processing request:", error);
     res.status(500).json({ message: 'Error processing request' });
